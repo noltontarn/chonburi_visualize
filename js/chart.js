e8f8b2857e1ecff0473c5cbbb65872c0a1a39e3c
noltontarn <noltontarn@gmail.com>
2019-05-13 12:18:59 +0700

    add linechart2 and css style

--- js/chart.js
+++ js/chart.js
@@ -1,5 +1,7 @@
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawLineChart1);
+google.charts.setOnLoadCallback(drawLineChart2);
+
 function drawLineChart1() {
     var options = {
         //title: 'Budget and Revenue in tourism',
@@ -12,6 +14,23 @@ function drawLineChart1() {
     var chart = new google.visualization.LineChart(document.getElementById('linechart1'));
 
     $.get("./data/linechart1.csv", function(csvString) {
+        var arrayData = $.csv.toArrays(csvString, {onParseValue: $.csv.hooks.castToScalar});
+        var data = google.visualization.arrayToDataTable(arrayData);
+        chart.draw(data, options);
+    });
+}
+
+function drawLineChart2() {
+    var options = {
+        legend: { position: 'bottom' },
+        hAxis: {title: 'Year'},
+        vAxis: {title: 'Millions Baht'},
+        colors: ['#ccebf9']   
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('linechart2'));
+
+    $.get("./data/linechart2.csv", function(csvString) {
         var arrayData = $.csv.toArrays(csvString, {onParseValue: $.csv.hooks.castToScalar});
         var data = google.visualization.arrayToDataTable(arrayData);
         chart.draw(data, options);
